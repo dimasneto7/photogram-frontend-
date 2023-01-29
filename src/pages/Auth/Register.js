08c394fda2d02c8e9620b9d910804d06b193a481
Dimas Neto <dimas_fcn@outlook.com>
2023-01-28 23:31:46 -0300

    register css

---
+++ src/pages/Auth/Register.js
@@ -0,0 +1,29 @@
+import "./Auth.css";
+
+import { Link } from "react-router-dom";
+
+import { useState, useEffect } from "react";
+
+const Register = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div>
+      <h2>PhotoGram</h2>
+      <p className="subtitle">Cadastre-se para ver as fotos dos seus amigos</p>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Nome" />
+        <input type="email" placeholder="E-mail" />
+        <input type="password" placeholder="Senha" />
+        <input type="password" placeholder="Confirme a senha" />
+        <input type="submit" value="Cadastrar" />
+      </form>
+      <p>
+        Já tem conta? <Link to="/login">clique aqui.</Link>
+      </p>
+    </div>
+  );
+};
+
+export default Register;
