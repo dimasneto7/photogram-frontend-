e18fc56b2a823b593518bbc826a937448354994f
Dimas Neto <dimas_fcn@outlook.com>
2023-01-30 21:45:58 -0300

    login

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -32,7 +32,9 @@ const Navbar = () => {
 
   return (
     <nav id="nav">
-      <Link to="/">PhotoGram</Link>
+      <Link to="/">
+        <h3>PhotoGram</h3>
+      </Link>
       <form id="search-form">
         <BsSearch />
         <input type="text" placeholder="Pesquisar" />
--- src/pages/Auth/Login.js
+++ src/pages/Auth/Login.js
@@ -1,9 +1,61 @@
 import "./Auth.css";
 
+import { Link } from "react-router-dom";
+import Message from "../../components/Message";
+
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+import { login, reset } from "../../slices/authSlice";
+
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const dispatch = useDispatch();
+
+  const { loading, error } = useSelector((state) => state.auth);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const user = {
+      email,
+      password,
+    };
+
+    dispatch(login(user));
+  };
+
+  // clean all auth states
+  useEffect(() => {
+    dispatch(reset());
+  }, [dispatch]);
+
   return (
     <div id="login">
-      <h1>Login</h1>
+      <h2>PhotoGram</h2>
+      <p className="subtitle">Faça o login para entrar na aplicação</p>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="E-mail"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email || ""}
+        />
+        <input
+          type="password"
+          placeholder="Senha"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password || ""}
+        />
+        {!loading && <input type="submit" value="Entrar" />}
+        {loading && <input type="submit" value="Aguarde..." disabled />}
+        {error && <Message msg={error} type="error" />}
+      </form>
+      <p>
+        Não tem uma conta? <Link to="/register">Clique aqui</Link>
+      </p>
     </div>
   );
 };
